4b6111e389dec8df5eb9faa6b018be43319b282c
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-09 10:27:39 -0300

    Realizado alguns ajustes

--- attractions/apps/core/models.py
+++ attractions/apps/core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from attractions.apps.attraction.models import Attraction
 
 
 class TouristSpot(models.Model):
     name = models.CharField(verbose_name='Nome', max_length=150)
     description = models.TextField(verbose_name='Descrição')
     status = models.BooleanField(verbose_name='Aprovado', default=False)
+    attractions = models.ManyToManyField(Attraction, verbose_name='Atrações')
 
     def __str__(self):
         return self.name
