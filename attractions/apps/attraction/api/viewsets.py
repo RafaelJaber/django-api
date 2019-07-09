c24f95fae81ad196cfd9fdc027c69aa05db77d8f
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-09 16:30:25 -0300

    Filtrando os dados na API pelo seguinte link na documentação: https://www.django-rest-framework.org/api-guide/filtering/

--- attractions/apps/attraction/api/viewsets.py
+++ attractions/apps/attraction/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from ..models import Attraction
 from .serializers import AttractionSerializer
@@ -7,4 +8,7 @@ class AttractionViewSet(ModelViewSet):
 
     queryset = Attraction.objects.all()
     serializer_class = AttractionSerializer
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('name', 'description')
+    ordering_fields = ('name', 'description')
 
--- attractions/settings.py
+++ attractions/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'attractions.apps.core',
     'attractions.apps.attraction',
     'attractions.apps.comments',
@@ -127,3 +128,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = 'images'
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
