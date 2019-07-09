beed47bd274751c45a1d4f0cc76c10b671e91dcb
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-09 14:10:10 -0300

    Endpoint de Attraction, criado

---
+++ attractions/apps/attraction/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from ..models import Attraction
+
+
+class AttractionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attraction
+        fields = (
+            'id', 'name', 'description', 'operating_hours', 'minimum_age'
+        )
--- attractions/urls.py
+++ attractions/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from attractions.apps.core.api.viewsets import TouristSpotViewSet
+from attractions.apps.attraction.api.viewsets import AttractionViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'tourist-spot', viewset=TouristSpotViewSet)
+router.register(r'attraction', viewset=AttractionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
